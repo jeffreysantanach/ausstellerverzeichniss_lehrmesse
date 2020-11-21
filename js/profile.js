695eb88b2af6b0793693fc102355b24a05d98a4c
Jeffrey Santana <accounts@jeffreysantana.ch>
2020-11-21 17:42:13 +0100

    working on profile page :construction:

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,34 @@
-function getCompany() {
-    var result = runGETRequest('https://lehrmesse.ch/api/company/?id=2');
+$(document).ready(function() {
+    var id = 3;
+    var company = getCompany(id);
+    console.log(company);
+
+
+    var booth = 'Stand: ' + company.booth;
+    var name = (company.name).bold();
+    var place = company.postalcode + ' ' + company.city;
+    var email = 'E-Mail: ' + company.email;
+    var tel = 'Tel: ' + company.phone;
+    var maps = 'https://maps.google.de/maps?hl=de&q=' + company.street + company.city + 'h&t=&z=14&ie=utf8&iwloc=b&output=embed';
+    var logo = 'img/logos/' + id + '.png';
+
+    $('#name').html(name);
+    $('#name_address').html(company.name)
+    $('#booth').html(booth);
+    $('#street').html(replaceletter(company.street));
+    $('#place').html(place);
+    $('#email').html(email);
+    $('#tel').html(tel);
+    $('#maps').attr("src", maps);
+    $('#logo').attr("src", logo)
+    $('#description').html(company.description);
+
+});
+
+
+function getCompany(id) {
+    var url = 'https://lehrmesse.ch/api/company/?id=' + id;
+    var result = runGETRequest(url);
     return result;
 
 }
@@ -11,4 +40,26 @@ function runGETRequest(url) {
     xmlHttp.send(null);
     data = JSON.parse(xmlHttp.responseText);
     return data;
+}
+
+function replaceletter(string) {
+    if (string.includes('ue')) {
+        string = string.replace('ue', '&uuml;');
+        return string;
+    } else if (string.includes('ae')) {
+        string = string.replace('ae', '&auml;');
+        return string;
+    } else if (string.includes('oe')) {
+        string = string.replace('oe', '&ouml;');
+        return string;
+    } else if (string.includes('Oe')) {
+        string = string.replace('Oe', '&Ouml;');
+        return string;
+    } else if (string.includes('Ae')) {
+        string = string.replace('Ae', '&Auml;');
+        return string;
+    } else if (string.includes('Ue')) {
+        string = string.replace('Ue', '&Uuml;');
+        return string;
+    }
 }
